d597f98f2628c55fe533ac3afa1dee5cc48435df
Revvilon <55360334+Revvilon@users.noreply.github.com>
2026-06-29 14:00:02 +0200

    - complete rewrite

--- old/v1/Util.java
+++ old/v1/Util.java
@@ -131,7 +131,6 @@ public class Util {
         if ((System.currentTimeMillis() - logs.get(text)) <= 2000) return;
         Util.sendMsg(Component.literal(text).withStyle(ChatFormatting.RED, ChatFormatting.BOLD));
         logs.replace(text, time);
-
-
     }
+
 }
