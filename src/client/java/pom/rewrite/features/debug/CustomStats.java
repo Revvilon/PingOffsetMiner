5082a951df8fa3fff6f088f24077c745f71d5a85
Revvilon <55360334+Revvilon@users.noreply.github.com>
2026-06-30 10:09:52 +0200

    fixed orbit implementation causing crash on startup

--- src/client/java/pom/rewrite/features/debug/CustomStats.java
+++ src/client/java/pom/rewrite/features/debug/CustomStats.java
@@ -5,7 +5,7 @@ import pom.rewrite.config.settings.SettingInt;
 import pom.rewrite.config.settings.SettingNum;
 
 public class CustomStats {
-    public static final Feature instance = new Feature("customStats", true);
+    public static final Feature instance = new Feature("customStats", false);
     public static final SettingInt customSpeed = new SettingInt(6000, "customSpeed", instance);
     public static final SettingInt customPing = new SettingInt(100, "customPing", instance);
     public static final SettingNum customTps = new SettingNum(20.0, "customTps", instance);
--- src/client/java/pom/rewrite/features/debug/Logging.java
+++ src/client/java/pom/rewrite/features/debug/Logging.java
@@ -4,7 +4,7 @@ import pom.rewrite.config.Feature;
 import pom.rewrite.config.settings.SettingInt;
 
 public class Logging {
-    public static final Feature instance = new Feature("logging", true);
+    public static final Feature instance = new Feature("logging", false);
     public static final Feature igetit = new Feature("igetit", false);
 
     public static final SettingInt xPos = new SettingInt(30, "xPos", instance);
