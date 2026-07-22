40b045df24d3cb49b69f69683af89702ed32e5a5
Revvilon <55360334+Revvilon@users.noreply.github.com>
2026-07-22 18:39:29 +0200

    added sound util class to event listener

--- src/client/java/pom/rewrite/PingOffsetMinerClient.java
+++ src/client/java/pom/rewrite/PingOffsetMinerClient.java
@@ -21,6 +21,7 @@ import pom.rewrite.utility.block.BlockUtil;
 import pom.rewrite.utility.render.RenderClass;
 import pom.rewrite.utility.server.IslandUtils;
 import pom.rewrite.utility.server.ServerStats;
+import pom.rewrite.utility.sound.SoundUtil;
 import pom.rewrite.utility.stats.EfficiencyStats;
 import pom.rewrite.utility.stats.MiningStats;
 import pom.rewrite.utility.stats.StatsReader;
@@ -89,5 +90,6 @@ public class PingOffsetMinerClient implements ClientModInitializer {
         EVENT_BUS.subscribe(EfficiencyStats.getInstance());
         EVENT_BUS.subscribe(MiningStats.instance());
         EVENT_BUS.subscribe(ServerStats.class);
+        EVENT_BUS.subscribe(new SoundUtil());
     }
 }
