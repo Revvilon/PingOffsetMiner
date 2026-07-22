e0ea63a0d88cc067dac2446508351fb15ec01a2c
Revvilon <55360334+Revvilon@users.noreply.github.com>
2026-07-22 18:26:39 +0200

    moved feature

---
+++ src/client/java/pom/rewrite/features/debug/PrecisionMining.java
@@ -0,0 +1,8 @@
+package pom.rewrite.features.debug;
+
+import pom.rewrite.config.Feature;
+import pom.rewrite.config.settings.SettingBoolean;
+
+public class PrecisionMining {
+    public static final Feature precisionMining = new Feature("precisionMining", true);
+}
